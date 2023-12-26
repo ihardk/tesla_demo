9659a10c6312bdd6c4a85882146c92cdf06dca48
Rahul Vaghela <96824603+rv1511@users.noreply.github.com>
2023-12-26 12:25:53 +0530

    save rahul slider

--- lib/screens/climate_screen.dart
+++ lib/screens/climate_screen.dart
@@ -51,57 +51,131 @@ class ClimateScreen extends StatelessWidget {
         decoration: const BoxDecoration(
           gradient: homeBGLinear,
         ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Positioned(
-              top: SizeConfig.blockSizeHorizontal * 60,
-              // right: SizeConfig.blockSizeHorizontal * 8,
-              child: const RoundedProgress(icon: setting),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Gap(100),
-                Container(
-                  // height: SizeConfig.screenHeight * 0.345,
-                  // padding: const EdgeInsets.all(20),
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
-                  child: SizedBox(
-                    height: SizeConfig.screenHeight * 0.08,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Ac",
-                            style: TextStyle(
-                              color: const Color(0x80EBEBF5),
-                              fontSize: SizeConfig.blockSizeHorizontal * 4.5,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          const Gap(15),
-                          const RoundedIconButton(icon: setting),
-                          const Gap(15),
-                          Expanded(
-                              child: Text(
-                            "Slider",
-                            style: TextStyle(
-                              color: const Color(0x50EBEBF5),
-                              fontSize: SizeConfig.blockSizeHorizontal * 4,
-                            ),
-                          )),
-                        ]),
-                  ),
-                ),
-              ],
-            ),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Gap(200),
+            RoundedProgress(icon: setting),
+            Gap(50),
+            SliderUnit(title: "Ac", icon: setting),
+            SliderUnit(title: "Fan", icon: setting),
+            SliderUnit(title: "Heat", icon: setting),
+            SliderUnit(title: "Auto", icon: setting),
           ],
         ),
       ),
+    );
+  }
+}
+
+class SliderUnit extends StatefulWidget {
+  final String title;
+  final String icon;
+  const SliderUnit({
+    super.key,
+    required this.title,
+    required this.icon,
+  });
+
+  @override
+  State<SliderUnit> createState() => _SliderUnitState();
+}
+
+class _SliderUnitState extends State<SliderUnit> {
+  // final double _value = 0.0;
+  // final bool _isDragging = false;
+  // final bool _isSlideCompleted = false;
+
+  // late AnimationController _controller;
+  // late Animation<double> _animation;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          // height: SizeConfig.screenHeight * 0.345,
+          // padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: SizeConfig.screenWidth * 0.1,
+                  child: Text(
+                    widget.title,
+                    style: TextStyle(
+                      color: const Color(0x80EBEBF5),
+                      fontSize: SizeConfig.blockSizeHorizontal * 4.5,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                const Gap(15),
+                RoundedIconButton(icon: widget.icon),
+                const Gap(25),
+                Expanded(
+                    child: Container(
+                  height: SizeConfig.screenHeight * 0.002,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    // color: const Color(0xFF1C1D20),
+                    // color: Colors.red,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.white30,
+                        blurRadius: 1.5,
+                        spreadRadius: -0.5,
+                        offset: Offset(0, 2),
+                      ),
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 1,
+                        spreadRadius: 0.2,
+                        offset: Offset(0, 0),
+                      )
+                    ],
+                  ),
+                )),
+              ]),
+        ),
+        Positioned(
+          left: SizeConfig.blockSizeHorizontal * 40,
+          child: Container(
+            height: SizeConfig.screenHeight * 0.015,
+            width: SizeConfig.screenWidth * 0.07,
+            decoration: BoxDecoration(
+              // color: Colors.red,
+              boxShadow: const [
+                // BoxShadow(
+                //   color: Colors.white30,
+                //   blurRadius: 1.5,
+                //   spreadRadius: -0.5,
+                //   offset: Offset(0, 2),
+                // ),
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 5,
+                  spreadRadius: 3,
+                  offset: Offset(2, 2),
+                )
+              ],
+              gradient: const LinearGradient(
+                colors: [
+                  Color(0xFF2E3236),
+                  Color(0xFF141515),
+                ],
+                stops: [0, 1],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
